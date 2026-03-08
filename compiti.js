39332281dd677e3933ce847e7ce17074b0eef220
FiliDalzi <fili.dalzi@gmail.com>
2026-03-08 12:18:28 +0100

    Update compiti.js

--- compiti.js
+++ compiti.js
@@ -26,10 +26,10 @@ async function aggiornaCompiti() {
 
   const browser = await puppeteer.launch({
     headless: true,
-    executablePath: '/opt/render/.cache/puppeteer/chrome/linux-146.0.7680.31/chrome-linux64/chrome',
     args: [
       '--no-sandbox',
-      '--disable-setuid-sandbox'
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage'
     ]
   });
 
